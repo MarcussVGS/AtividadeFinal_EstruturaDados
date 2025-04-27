8cd50bf5133d7524482672130bf39d62b03264d0
Marcus Machine <Marcus Machine@DESKTOP-SF160U6>
2025-04-27 15:57:24 -0300

    Limpeza de comentarios e criação do Main

--- src/Aplicacao_JogoLabirinto/Aplicacao_JogoLabirinto.java
+++ src/Aplicacao_JogoLabirinto/Aplicacao_JogoLabirinto.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Aplicacao_JogoLabirinto;
+//@author Marcus Machine
 
-/**
- *
- * @author Marcus Machine
- */
 public class Aplicacao_JogoLabirinto {
+    public static void main(String[] args) {
+        
+    }
     
 }
