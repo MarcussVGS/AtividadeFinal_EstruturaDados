7bb43f352e63445818b539c74bb33161b244cb05
Marcus Machine <Marcus Machine@DESKTOP-SF160U6>
2025-04-27 17:22:27 -0300

    novos metodos na classe cobra, caminho e print caminho

--- src/Aplicacao_JogoLabirinto/Aplicacao_JogoLabirinto.java
+++ src/Aplicacao_JogoLabirinto/Aplicacao_JogoLabirinto.java
@@ -1,8 +1,16 @@
 package Aplicacao_JogoLabirinto;
 //@author Marcus Machine
 
+import JogoLabirinto.*;
+
 public class Aplicacao_JogoLabirinto {
     public static void main(String[] args) {
+        
+        Cobra cb = new Cobra ();
+        
+        cb.Caminho(0, 0);
+        cb.Caminho(1, 0);
+        cb.Caminho(2, 0);
         
     }
     
--- src/JogoLabirinto/Cobra.java
+++ src/JogoLabirinto/Cobra.java
@@ -1,6 +1,26 @@
 package JogoLabirinto;
 //@author Marcus Machine
+import java.util.Stack;
+
 
 public class Cobra {
+    Stack<Coordenada> pilhaCaminho = new Stack<>();
+    
+    public Cobra(){
+    }
+    
+    public boolean Caminho (int linha, int coluna){
+        //teste adicionar coordenadas
+        pilhaCaminho.push(new Coordenada(linha, coluna) );
+        return true;
+    }
+    
+    public boolean printCaminho (){
+        while (!pilhaCaminho.empty() ){
+            System.out.println("Caminho:"); 
+            System.out.println("\n" + pilhaCaminho.get(0) );
+        }
+        return true;
+    }
     
 }
