45b76116a8fe85c2b63181c0fb5143e835524705
Marcus Machine <Marcus Machine@DESKTOP-SF160U6>
2025-04-27 16:11:36 -0300

    Criação de contrutor, duas classes de lariinto e metodo get

--- src/JogoLabirinto/Labirinto.java
+++ src/JogoLabirinto/Labirinto.java
@@ -2,5 +2,39 @@ package JogoLabirinto;
 //@author Marcus Machine
 
 public class Labirinto {
+    private int[][] labirinto;
+    
+// construtor ja cria um laberinto
+    public Labirinto(int num){
+        if (num == 1){ criarLabirinto3x5(); }
+        if (num == 2){ criarLabirinto5x5(); }
+    }
+    
+    //labirinto de codigo 1
+    public boolean criarLabirinto3x5 (){
+        labirinto = new int[][]{
+            {1,1,1,1,1},
+            {2,0,0,0,3},
+            {1,1,1,1,1}
+        };
+        return true;
+    }
+    
+    //labirinto de codigo 2
+    public boolean criarLabirinto5x5 (){
+        labirinto = new int[][]{
+            {1,1,1,1,1},
+            {1,0,0,0,1},
+            {1,0,1,0,1},
+            {1,0,1,0,1},
+            {1,2,1,3,1}
+        };
+        return true;
+    }
+
+    
+    public int[][] getLabirinto() {
+        return labirinto;
+    }
     
 }
